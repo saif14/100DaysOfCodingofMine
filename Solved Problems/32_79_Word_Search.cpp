c43e69cb1edffbeb2133b5324d52c4913152ec85
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2021-10-24 15:11:34 +0600

    finally solved 32nd problem. A f*** problem!

--- Solved Problems/32_79_Word_Search.cpp
+++ Solved Problems/32_79_Word_Search.cpp
@@ -2,10 +2,44 @@
 
 using namespace std;
 
+bool dfs(vector<vector<char>> &board, string &word, int i, int j, int idx, vector<vector<bool>> &visited)
+{
+    if (idx == word.length() - 1)
+        return true;
+    visited[i][j] = true;
+    if (i > 0 && !visited[i - 1][j] && board[i - 1][j] == word[idx+1] && dfs(board, word, i - 1, j, idx + 1, visited))
+        return true;
+    if(i < board.size()-1 && !visited[i+1][j] && board[i+1][j] == word[idx+1] && dfs(board, word, i+1, j, idx+1, visited))
+    return true;
+    if(j >0 && !visited[i][j-1] && board[i][j-1] == word[idx+1] && dfs(board, word, i, j-1, idx+1, visited))
+    return true;
+    if(j< board[0].size() -1 && !visited[i][j+1] && board[i][j+1] == word[idx+1] && dfs(board, word, i, j+1, idx+1, visited))
+    return true;
+    visited[i][j] = false;
+    return false;
+
+}
+
 bool exist(vector<vector<char>> &board, string word)
 {
+    int r = board.size();
+    int c = board[0].size();
+
+    vector<vector<bool>> visited(r, vector<bool>(c, false));
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (board[i][j] == word[0] && dfs(board, word, i, j, 0, visited))
+                return true;
+        }
+    }
+    return false;
 }
 
 int main()
 {
+    vector<vector<char>> board {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    cout<<exist(board, "ABCCED");
 }
