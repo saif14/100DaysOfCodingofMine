51a75ba5c479b365095205652b912a66dc0bf8f4
saif14 <mahmudalsaif@gmail.com>
2022-01-24 21:32:58 +0600

    tweak a bit

--- ProblemPracticeforJob/test.c
+++ ProblemPracticeforJob/test.c
@@ -1,8 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void  reverseString(char *str)
+{
+    int startIdx = 0;
+    int endIdx = strlen(str)-1;
+
+    while (startIdx < endIdx)
+    {
+        str[startIdx] ^= str[endIdx];
+        str[endIdx] ^= str[startIdx];
+        str[startIdx] ^= str[endIdx];
+        startIdx++;
+        endIdx--;
+    }
+
+}
 
 
 int main()
 {
-    printf("%c",'A'+32);
+    char str[] = "ABCD";
+    reverseString(str);
+
+    if(!strcmp(str, "DCBA"))
+    {
+        printf("Hello World!");
+    }
+    
+    
 }
