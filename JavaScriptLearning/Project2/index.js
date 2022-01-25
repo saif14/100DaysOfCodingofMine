93fc9f59f5e3aedb41ae5735aedfe7a942482d58
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-01-25 18:02:43 +0600

    completed js project 2. solved today's problem

--- JavaScriptLearning/Project2/index.js
+++ JavaScriptLearning/Project2/index.js
@@ -1,33 +1,56 @@
 let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
-// Get the leads from the localStorage - PS: JSON.parse()
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
-localStorage.clear()
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// const tabs = [{ url: 'https://www.linkedin.com/in/per-harald-borgen/' }]
 
-console.log(leadsFromLocalStorage)
+function deleteBtnAction() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function() {
+function tabBtnAction() {
+    // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+
+    // })
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+}
+
+deleteBtn.addEventListener('dblclick', deleteBtnAction)
+tabBtn.addEventListener('click', tabBtnAction)
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
 
     // To verify that it works:
-    console.log(localStorage.getItem("myLeads"))
+    console.log(localStorage.getItem('myLeads'))
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+function render(leads) {
+    let listItems = ''
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href="${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>
         `
