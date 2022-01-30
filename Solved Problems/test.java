f8f048c5bb1ef93374d8ec86876605f778b266cb
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-01-30 13:34:26 +0600

    solved 67th problem

--- Solved Problems/test.java
+++ Solved Problems/test.java
@@ -1,8 +1,21 @@
-class test{
-    public static void main(String[] args){
-        
+class test {
+
+    int x = 100;
+
+    void testM() {
+        inner i = new inner();
+        i.show();
     }
 
-    
-   
+    class inner {
+        void show(){
+            System.out.println("outer value: "+ x);
+        }
+    }
+
+    public static void main(String[] args) {
+        test t = new test();
+        t.testM();;
+    }
+
 }
