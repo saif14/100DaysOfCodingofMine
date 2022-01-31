65647de23b9d43ef751ca3d6fa039890531a8123
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-01-31 16:48:14 +0600

    learning recursion. mostly solved 69th

--- Solved Problems/Test.cpp
+++ Solved Problems/Test.cpp
@@ -2,26 +2,8 @@
 
 using namespace std;
 
-void rotate(vector<int> &nums, int k)
-{
 
-    int n = nums.size();
-    k = (k%n);
-    cout<<"k "<<k<<endl;
-    vector<int> newNums(n,0);
-    for (int i = 0; i < n; i++)
-    {
-        newNums[i] = nums[(n - k) % n];
-        k--;
-    }
-    nums = newNums;
-}
 int main()
 {
-    vector<int> nums {1,2,3,4,5,6,7};
-    rotate(nums, 3);
-    for (int i : nums)
-    {
-        cout << i << " ";
-    }
+   
 }
