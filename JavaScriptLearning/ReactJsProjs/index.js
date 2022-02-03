6af0b957b12213953ce972176823012f7d73648b
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-02-03 12:42:32 +0600

    learned and solved 70th problem

--- JavaScriptLearning/ReactJsProjs/index.js
+++ JavaScriptLearning/ReactJsProjs/index.js
@@ -1,4 +1,4 @@
-import Header from "./Header.js"
+import Header from "./Header"
 
 const page = (
   <div>
--- Solved Problems/9_TwoSum.cpp
+++ Solved Problems/9_TwoSum.cpp
@@ -2,16 +2,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+/**
+ * O(nlogn) Complexity and O(n) -->space complexity
+ */
 
 vector<int> twoSum(vector<int> &nums, int target)
 {
-    //it can be done in 3 ways. Need to learn this
+    int n = nums.size();
+
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++)
+        v.push_back({nums[i], i});
+    sort(begin(v), end(v));
+    int left = 0, right = n - 1;
+    while (left < right)
+    {
+        int sum = v[left].first + v[right].first;
+        if (sum == target)
+            return {v[left].second, v[right].second};
+        else if (sum > target)
+            right--;
+        else
+            left++;
+    }
+}
+
+/**
+ * O(n) complexity
+ * */
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        int key = target - nums[i];
+        if (mp.find(key) != mp.end())
+            return {mp[key], i};
+        mp[nums[i]] = i;
+    }
+    return {};
 }
 
 int main()
 {
-    vector<int> nums = {2,7,11,15};
+    vector<int> nums = {2, 7, 11, 15};
     vector<int> res = twoSum(nums, 9);
-    cout<<"["<<res[0]<<","<<res[1]<<"]"<<endl;
+    cout << "[" << res[0] << "," << res[1] << "]" << endl;
 }
