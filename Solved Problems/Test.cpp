06af8f716b363690f612074aae420182b7520c36
saif14 <mahmudalsaif@gmail.com>
2022-02-05 21:08:55 +0600

    already solved

--- Solved Problems/Test.cpp
+++ Solved Problems/Test.cpp
@@ -2,28 +2,85 @@
 
 using namespace std;
 
-int findMaxLength(vector<int> &nums)
+struct ListNode
 {
-    map<int, int> m;
-    int res = 0;
-    int sum = 0;
-    for(int i=0; i<nums.size(); i++)
-    {
-        sum += (nums[i]==0?-1:1);
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-        if(sum == 0)
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+{
+    if (l1 == NULL && l2 == NULL)
+        return NULL;
+    ListNode *resultList = new ListNode();
+    ListNode *temp;
+    temp = resultList;
+    while (l1 && l2)
+    {
+
+        if (l1->val <= l2->val)
         {
-            res = max(res,i-0+1);
-        }
-        else if(m.find(sum) != m.end())
-        {
-            res = max(res, i-m[sum]);
+            temp->val = l1->val;
+            cout << "l1: " << l1->val << " t: " << temp->val << endl;
+            l1 = l1->next;
+            temp->next = new ListNode();
+            temp = temp->next;
         }
         else
         {
-            m[sum] = i;
+            // cout<<l2->val;
+            temp->val = l2->val;
+            cout << "l2: " << l2->val << " t: " << temp->val << endl;
+            l2 = l2->next;
+            temp->next = new ListNode();
+            temp = temp->next;
         }
+    }
+    while (l1)
+    {
+        temp->val = l1->val;
+        cout << "l3: " << l1->val << " t: " << temp->val << endl;
+        l1 = l1->next;
+        if (l1 != NULL)
+            temp->next = new ListNode();
+        temp = temp->next;
+    }
+    while (l2)
+    {
+        // cout<<"l4: "<<l2->val<<endl;
+        temp->val = l2->val;
+        // cout<<"temp: "<<l2->val<<endl;
+        l2 = l2->next;
+        // cout<<"l4: "<<l2->val<<" t: "<<temp->val<<endl;
+        if (l2 != NULL)
+            temp->next = new ListNode();
+        temp = temp->next;
+    }
 
+    return resultList;
+}
+
+ListNode *mergeKLists(vector<ListNode *> &lists)
+{
+
+    if (lists.size() == 0)
+    {
+        return NULL;
+    }
+    if (lists.size() == 1)
+    {
+        return lists[0];
+    }
+    int sz = lists.size();
+
+    ListNode *res = lists[0];
+
+    for (int i = 1; i < sz; i++)
+    {
+        res = mergeTwoLists(res, lists[i]);
     }
 
     return res;
