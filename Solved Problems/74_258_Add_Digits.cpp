422869dc194d46cd8c639d5569524f014d03656a
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-02-08 10:11:25 +0600

    leaarned about some number theory and solved 74th problem

--- Solved Problems/74_258_Add_Digits.cpp
+++ Solved Problems/74_258_Add_Digits.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int addDigits(int num)
 {
-    if(num == 0 ) return 0;
-    return num%9 == 0 ? 9 : num%9;
-
+    if (num == 0)
+        return 0;
+    return num % 9 == 0 ? 9 : num % 9;
 }
 
 int main()
