7b3fbaf1e7abc6788c0b09f7d15e9bf0f4f1c5e7
Saif Mahmud Parvez <mahmmudalsaif@gmail.com>
2022-02-09 18:52:09 +0600

    learned react more

--- JavaScriptLearning/dummy-air-bnb/src/App.js
+++ JavaScriptLearning/dummy-air-bnb/src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import Card from "./components/card"
+import cardData from "./components/data"
 
 function App() {
-  return (
-   <div>
-     <Navbar/>
-     <Hero/>
-     <Card 
-     img="katie-zaferes.png"
-     rating="5.0"
-     reviewCount = "6"
-     country = "America"
-     title = "Life Lesson with Katie Zeferes"
-     price = {136}
-     />
-   </div>
-  );
+    const cardElements = cardData.map((card) => {
+        return <Card
+            id={card.id}
+            title={card.title}
+            description={card.description}
+            price={card.price}
+            coverImg={card.coverImg}
+            rating={card.stats.rating}
+            reviewCount={card.stats.reviewCount}
+            location={card.location}
+            openSpots={card.openSpots}
+        />
+    })
+    console.log(cardElements);
+    return (<div>
+        <Navbar />
+        <Hero />
+
+        {cardElements}
+
+    </div>
+    );
 }
 
 export default App;
--- JavaScriptLearning/dummy-air-bnb/src/components/card.js
+++ JavaScriptLearning/dummy-air-bnb/src/components/card.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Card(cardProps) {
   return (
     <div className="card">
-      <img src={`../images/${cardProps.img}`} class="card-img"></img>
+      <img src={`../images/${cardProps.coverImg}`} class="card-img"></img>
       <div className="card-stats">
         <img src="../images/star.png" className="card-star"></img>
         <span>{cardProps.rating}</span>
         <span className="gray">({cardProps.reviewCount}) •</span>
-        <span className="gray">{cardProps.country}</span>
+        <span className="gray">{cardProps.location}</span>
       </div>
       <p>{cardProps.title}</p>
       <p> <span className="bold">From ${cardProps.price}</span> / person</p>
