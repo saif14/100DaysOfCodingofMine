a9e1f2096f7fabc7c96328994f8ed8e85c442993
saif14 <mahmudalsaif@gmail.com>
2022-02-15 18:55:40 -0800

    test

--- Solved Problems/Test.cpp
+++ Solved Problems/Test.cpp
@@ -1,18 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-double myPow(double x, int n)
+int singleNumber(vector<int>& nums)
 {
-    if (n == 0)
-        return 1;
-    if (n > 0)
-        return x * myPow(x, n - 1);
-    if (n < 0)
-        return 1 / x * myPow(x, n + 1);
+
 }
 
 int main()
 {
-    cout << myPow(0.00001,2147483647);
+    cout<<"hello";
 }
