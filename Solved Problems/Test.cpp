a4049454d72509d74643d17c37c7da8461bacee0
saif14 <mahmudalsaif@gmail.com>
2022-05-30 11:44:10 +0600

    added js files

--- Solved Problems/Test.cpp
+++ Solved Problems/Test.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-int titleToNumber(string columnTitle)
-{
-    int colNum = 0;
-    for(int i=0; i<columnTitle.length(); i++)
-    {
-        colNum += columnTitle[i] - 'A' + 1;
-    }
-
-    return colNum;
-}
 
 int main()
 {
