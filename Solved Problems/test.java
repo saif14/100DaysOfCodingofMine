b21ddbfb6cccc1c4af8cd84e74b0e5e82c45f33e
saif14 <mahmudalsaif@gmail.com>
2022-08-14 17:42:08 +0600

    tried 1888 problem in test.java

--- Solved Problems/test.java
+++ Solved Problems/test.java
@@ -2,33 +2,37 @@ import java.util.Arrays;
 
 class test {
 
-    public static int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
-        int l=0, r=0;
-        int maxFreq = 0;
-        int total = 0;
-        int windowLen = 0;
-        while(r <= nums.length-1){
-            windowLen = r-l+1;
-            total+= nums[r];
-            if(nums[r]*windowLen <= total+k){
-                // total+=nums[r];
-                r++;
-                maxFreq = Math.max(maxFreq, windowLen);
-            }else{
-                total=total-nums[l];
-                l++;
-            }
+    public static int minFlips(String s) {
+        int ln = s.length();
+        s = s+ s;
+        int res = s.length();
+        String alt_1 = "";
+        String alt_0 = "";
+        int diff_1 = 0;
+        int diff_0 = 0;
+        int l = 0, r = 0;
+        for(int i = 0; i < s.length(); i++) {
+            alt_1 = (i%2 == 0) ? alt_1+"1" : alt_1+"0";
+            alt_0 = (i%2 == 0) ? alt_0+"0" : alt_0+"1";;
         }
 
-        return maxFreq;
+        for(;r<s.length(); r++){
+            if(s.charAt(r) != alt_1.charAt(r)) diff_1++;
+            if(s.charAt(r) != alt_0.charAt(r)) diff_0++;
+
+            if(r- l + 1 > ln){
+                if(s.charAt(l) != alt_1.charAt(l)) diff_1--;
+                if(s.charAt(l) != alt_0.charAt(l)) diff_0--;
+                l++;
+            }
+            if((r- l + 1) == ln) res = Math.min(res, Math.min(diff_1, diff_0));
+        }
+
+        return res;
+
     }
     public static void main(String[] args) {
-        
-        int[] a = {3,9,6};
-        int k = 2;
-        int sz = maxFrequency(a,k);
-        System.out.println(sz);
+        System.out.println(minFlips("111000"));
     }
 
 }
